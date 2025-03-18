6e6165a1a5a640af6589e0842b8a3d5e2cf42ab8
Abdumajid05 <abdumajidabdulazizov73@gmail.com>
2025-03-18 18:41:52 +0500

    update

--- flask_app.py
+++ flask_app.py
@@ -1,1 +1,13 @@
-f
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/get/sum')
+def main():
+    a=int(request.args.get('a'))
+    b=int(request.args.get('b'))
+    c=a+b
+    return {"result":c}
+
+if __name__ == '__main__':  
+    app.run(debug=True)
